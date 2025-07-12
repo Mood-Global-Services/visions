bd6135dfb6facaeb7dee224a35ff7e39b7fcca33
MedNewton <med.newton.2013@gmail.com>
2025-07-12 03:39:00 +0100

    .

--- components/checkout.tsx
+++ components/checkout.tsx
@@ -3,8 +3,8 @@
 import { CrossmintProvider, CrossmintEmbeddedCheckout } from "@crossmint/client-sdk-react-ui";
 
 export default function CheckoutWithCrossmint() {
-    const clientApiKey = process.env.CROSSMINT_STAGING_CLIENT_API_KEY as string;
-    const collectionId = process.env.STAGING_COLLECTION_ID as string;
+    const clientApiKey = process.env.NEXT_PUBLIC_CROSSMINT_STAGING_CLIENT_API_KEY as string;
+    const collectionId = process.env.NEXT_PUBLIC_STAGING_COLLECTION_ID as string;
 
     return (
         <div className="flex flex-col items-center justify-start h-screen p-6 bg-white">
